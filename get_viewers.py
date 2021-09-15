f7af4d9e7b1ec7589803cc7ccbba323651657dab
denn9s <cs.dennisong@gmail.com>
2021-09-14 22:27:19 -0700

    fixed success flag position and changed flag to integer

--- get_viewers.py
+++ get_viewers.py
@@ -4,11 +4,11 @@ import sys
 
 def main():
     # test_url = 'https://www.youtube.com/watch?v=yBBjqi-4gcs' 
-    chatter_data = {'success': False, 'unique_chatter_count': 0, 'chatter_list': []}
+    chatter_data = {'success': 0, 'unique_chatter_count': 0, 'chatter_list': []}
     try:
-        chatter_data['success'] = True
         input_url = sys.argv[1]
         chat = chat_downloader.ChatDownloader().get_chat(input_url)
+        chatter_data['success'] = 1
         chatter_list = set()
         for message in chat:
             chat.print_formatted(message)
