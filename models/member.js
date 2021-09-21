565dc7b0eaa9785351400e4cd1f072b8989b6be8
denn9s <cs.dennisong@gmail.com>
2021-09-20 21:58:40 -0700

    moved total_videos to update schema

--- models/member.js
+++ models/member.js
@@ -4,7 +4,6 @@ const MemberSchema = new mongoose.Schema({
     id: { type: String, required: true },
     name: { type: String, required: true },
     youtube_id: { type: String, required: true },
-    total_videos: {type: Number },
     group_data: {
         group_name: { type: String, required: true }, 
         generation_name: { type: String, required: true }
