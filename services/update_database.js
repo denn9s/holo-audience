8182b74d499e043b44dcbad2918610879363d5ed
denn9s <cs.dennisong@gmail.com>
2021-09-22 22:39:26 -0700

    added method to get array of new streams of member

--- services/update_database.js
+++ services/update_database.js
@@ -2,8 +2,7 @@ const mongoose = require('mongoose');
 const {PythonShell} = require('python-shell');
 
 const Member = require('../models/member');
-
-// const get_stream_info = require('../stream_data');
+const Stream = require('../models/stream');
 
 mongoose.connect('mongodb://localhost:27017/holo', { useNewUrlParser: true, useUnifiedTopology: true })
     .then(() => {
@@ -37,4 +36,15 @@ async function get_all_members() {
         member_id_array.push(mem.id);
     }
     return member_id_array;
+}
+
+async function get_new_streams(member_id) {
+    let current_stream_id_list = await get_all_streams(member_id);
+    let new_stream_id_list = [];
+    for (let stream_id of current_stream_id_list) {
+        if ((await Stream.exists({id: stream_id})) === false) {
+            new_stream_id_list.push(stream_id);
+        }
+    }
+    return new_stream_id_list;
 }
