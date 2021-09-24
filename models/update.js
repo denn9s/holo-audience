2ddc43e640fdb8010925acaf21674dd4e66534eb
denn9s <cs.dennisong@gmail.com>
2021-09-24 00:00:55 -0700

    renamed id to make more sense

--- models/update.js
+++ models/update.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose');
 
 const MemberUpdateSchema = new mongoose.Schema({
-    id: { type: String, required: true, unique: true },
+    member_id: { type: String, required: true, unique: true },
     total_videos: {type: Number },
     last_added_video_id: { type: String },
     last_added_video_date: { type: Date },
