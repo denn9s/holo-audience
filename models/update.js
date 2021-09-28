5c43bbebb42aea8819695a931767c6dc596aa39f
denn9s <cs.dennisong@gmail.com>
2021-09-27 21:00:13 -0700

    changed attributes names to match functionality

--- models/update.js
+++ models/update.js
@@ -3,8 +3,8 @@ const mongoose = require('mongoose');
 const MemberUpdateSchema = new mongoose.Schema({
     member_id: { type: String, required: true, unique: true },
     total_videos: {type: Number },
-    last_added_video_id: { type: String },
-    last_added_video_date: { type: Date },
+    most_recent_video_id: { type: String },
+    most_recent_video_date: { type: Date },
 });
 
 module.exports = mongoose.model("MemberUpdate", MemberUpdateSchema);
