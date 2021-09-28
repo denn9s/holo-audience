4c5a293d6e76e16ce70ab8af59b73a7d740e481c
denn9s <cs.dennisong@gmail.com>
2021-09-27 21:26:25 -0700

    created Mongoose schema to store intersections
    stores count of common chatters instead of array of viewers

---
+++ models/intersection.js
@@ -0,0 +1,9 @@
+const mongoose = require('mongoose');
+
+const Intersection = new mongoose.Schema({
+    first_stream_id: { type: String, required: true },
+    second_stream_id: { type: String, required: true },
+    common_count: { type: Number, required: true }
+});
+
+module.exports = mongoose.model("Intersection", Intersection);
