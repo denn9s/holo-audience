9873b9e8ab0dfb405da8f20fdac0fb073f509054
denn9s <cs.dennisong@gmail.com>
2021-09-27 22:05:23 -0700

    updated schema to contain member IDs

--- models/intersection.js
+++ models/intersection.js
@@ -2,7 +2,9 @@ const mongoose = require('mongoose');
 
 const Intersection = new mongoose.Schema({
     first_stream_id: { type: String, required: true },
+    first_stream_member_id: { type: String, required: true},
     second_stream_id: { type: String, required: true },
+    second_stream_member_id: { type: String, required: true},
     common_count: { type: Number, required: true }
 });
 
