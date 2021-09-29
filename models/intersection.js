8596af98560555fe73df7b89debb667e41e185fa
denn9s <cs.dennisong@gmail.com>
2021-09-28 23:26:30 -0700

    added compound index with stream ID pairs

--- models/intersection.js
+++ models/intersection.js
@@ -1,11 +1,13 @@
 const mongoose = require('mongoose');
 
 const Intersection = new mongoose.Schema({
-    first_stream_id: { type: String, required: true },
+    first_stream_id: { type: String, required: true, unique: false },
     first_stream_member_id: { type: String, required: true},
-    second_stream_id: { type: String, required: true },
+    second_stream_id: { type: String, required: true, unique: false },
     second_stream_member_id: { type: String, required: true},
     common_count: { type: Number, required: true }
 });
+
+Intersection.index({ first_stream_id: 1, second_stream_id: 1 }, { unique: true });
 
 module.exports = mongoose.model("Intersection", Intersection);
