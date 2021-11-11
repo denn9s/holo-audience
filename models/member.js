2327621545f5e0bedf12c7ba986e4dbd2f279961
denn9s <cs.dennisong@gmail.com>
2021-11-10 18:11:30 -0800

    updated Member schema with total streams

--- models/member.js
+++ models/member.js
@@ -4,6 +4,10 @@ const MemberSchema = new mongoose.Schema({
     id: { type: String, required: true, unique: true },
     name: { type: String, required: true },
     youtube_id: { type: String, required: true, unique: true },
+    total_streams: { 
+        type: Number,
+        min: [0, 'No negative stream count!']
+    },
     color: {
         red: { type: Number },
         green: { type: Number },
