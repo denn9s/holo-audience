e58de6ff1b0c6e1e4b6bb49b79f8704907607f74
denn9s <cs.dennisong@gmail.com>
2021-12-03 19:50:11 -0800

    allowed public connections to server
    not just localhost

--- server.js
+++ server.js
@@ -10,9 +10,7 @@ app.set('views', path.join(__dirname, 'views'));
 app.use(express.static("public"));
 
 app.use('', routes);
-// app.use(express.static(__dirname + '/public'));
-// app.use('/public', express.static('public'));
 
-app.listen(3000, () => {
+app.listen(3000, '0.0.0.0', () => {
     console.log('Listening on Port 3000');
 });
