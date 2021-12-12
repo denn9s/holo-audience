9d70b5a564a53570b411469bc01085bdcbe3cbbd
denn9s <cs.dennisong@gmail.com>
2021-12-11 20:01:19 -0800

    added cors

--- server.js
+++ server.js
@@ -1,8 +1,11 @@
 const express = require('express');
 const path = require('path');
 const routes = require('./routes');
+const cors = require('cors');
 
 const app = express();
+
+app.use(cors());
 
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
